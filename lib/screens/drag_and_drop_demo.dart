2c585a317f5d46c79afe3fc3592c6911d5279e85
martin_wang <martin_wang@ms.dlp.com.tw>
2024-05-17 17:17:50 +0800

    update card stack

--- lib/screens/drag_and_drop_demo.dart
+++ lib/screens/drag_and_drop_demo.dart
@@ -12,24 +12,24 @@ class DragAndDropDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [DraggableObject(), DraggableObjectTarget()],
+      children: [CardStack(), DraggableObjectTarget()],
     );
   }
 }
 
-class DraggableObject extends StatefulWidget {
-  const DraggableObject({super.key});
+class CardStack extends StatefulWidget {
+  const CardStack({super.key});
 
   @override
-  State<DraggableObject> createState() => _DraggableObjectState();
+  State<CardStack> createState() => _CardStackState();
 }
 
-class _DraggableObjectState extends State<DraggableObject> {
-  Widget CardBase() {
+class _CardStackState extends State<CardStack> {
+  Widget cardBase() {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         color: Colors.black45,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       width: 68.4,
       height: 102.4,
@@ -41,7 +41,7 @@ class _DraggableObjectState extends State<DraggableObject> {
     return Draggable(
       child: UnoCard('assets/uno/images/blue0.png'),
       feedback: UnoCard('assets/uno/images/blue0.png'),
-      childWhenDragging: CardBase(),
+      childWhenDragging: cardBase(),
       data: Colors.orange,
       onDragStarted: () {
         debugPrint('onDragStarted(), 開始拖動');
@@ -68,6 +68,8 @@ class DraggableObjectTarget extends StatefulWidget {
 
 class _DraggableObjectTargetState extends State<DraggableObjectTarget> {
   var _targetColor = Colors.blue;
+
+  List<UnoCard> cardsStack = [];
 
   @override
   Widget build(BuildContext context) {
@@ -129,44 +131,45 @@ class PlayingUnoCardState extends StatelessWidget {
   }
 }
 
-class BoardState {
-  List<UnoCard> allCards = [];
-  BoardState() {
-    initCards();
-  }
+class BoardState extends ChangeNotifier {
+  List<UnoCard> _cardsStack = [];
 
   void initCards() {
     for (var i = 0; i < 13; i++) {
       if (i < 10) {
-        allCards.add(UnoCard('assets/uno/images/blue$i.png'));
-        allCards.add(UnoCard('assets/uno/images/green$i.png'));
-        allCards.add(UnoCard('assets/uno/images/red$i.png'));
-        allCards.add(UnoCard('assets/uno/images/yellow$i.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/blue$i.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/green$i.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/red$i.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/yellow$i.png'));
       }
       if (i == 10) {
-        allCards.add(UnoCard('assets/uno/images/blueDrawTwo.png'));
-        allCards.add(UnoCard('assets/uno/images/greenDrawTwo.png'));
-        allCards.add(UnoCard('assets/uno/images/redDrawTwo.png'));
-        allCards.add(UnoCard('assets/uno/images/yellowDrawTwo.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/blueDrawTwo.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/greenDrawTwo.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/redDrawTwo.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/yellowDrawTwo.png'));
       }
       if (i == 11) {
-        allCards.add(UnoCard('assets/uno/images/blueReverse.png'));
-        allCards.add(UnoCard('assets/uno/images/greenReverse.png'));
-        allCards.add(UnoCard('assets/uno/images/redReverse.png'));
-        allCards.add(UnoCard('assets/uno/images/yellowReverse.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/blueReverse.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/greenReverse.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/redReverse.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/yellowReverse.png'));
       }
       if (i == 12) {
-        allCards.add(UnoCard('assets/uno/images/blueSkip.png'));
-        allCards.add(UnoCard('assets/uno/images/greenSkip.png'));
-        allCards.add(UnoCard('assets/uno/images/redSkip.png'));
-        allCards.add(UnoCard('assets/uno/images/yellowSkip.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/blueSkip.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/greenSkip.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/redSkip.png'));
+        _cardsStack.add(UnoCard('assets/uno/images/yellowSkip.png'));
       }
     }
 
     for (var i = 0; i < 4; i++) {
-      allCards.add(UnoCard('assets/uno/images/wild.png'));
-      allCards.add(UnoCard('assets/uno/images/wildDrawFour.png'));
+      _cardsStack.add(UnoCard('assets/uno/images/wild.png'));
+      _cardsStack.add(UnoCard('assets/uno/images/wildDrawFour.png'));
     }
+  }
+
+  UnoCard getCard() {
+    return _cardsStack.removeAt(0);
   }
 }
 
